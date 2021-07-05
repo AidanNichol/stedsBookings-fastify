fc23ec0f5a74047eb883d51361fed3f96a4cfcf2
Aidan Nichol <aidan@nicholware.co.uk>
2021-07-05 11:09:11 +0100

    v0.1.1

---
+++ deploy.mjs
@@ -0,0 +1,43 @@
+import ftp from 'basic-ftp';
+import logUpdate from 'log-update';
+
+example();
+
+async function example() {
+  const client = new ftp.Client();
+  let last = '';
+  client.trackProgress((info) => {
+    // console.log('File', info.name);
+    // console.log('Type', info.type);
+    // console.log('Transferred', info.bytes);
+    // console.log('Transferred Overall', info.bytesOverall);
+    if (last !== info.name) {
+      logUpdate.done();
+      last = info.name;
+    }
+    logUpdate(`File ${info.name},  Bytes ${info.bytes}, Total ${info.bytesOverall},`);
+  });
+  // client.ftp.verbose = true;
+  try {
+    await client.access({
+      host: 'ftp.stedwardsfellwalkers.co.uk',
+      user: 'vscode@stedwardsfellwalkers.co.uk',
+      password: 'SN5eaR!434QM5cmx',
+      secure: true,
+      port: 21,
+      secureOptions: { servername: 'ukhost4u.com' },
+    });
+    await client.ensureDir('/public_html/bookingsServer');
+    console.log(await client.pwd());
+    // await client.uploadFrom('database.sqlite', 'database.sqlite');
+    // await client.uploadFrom('.env', '.env');
+    // await client.uploadFrom('package.json', 'package.json');
+    await client.uploadFromDir('server', 'server');
+    await client.uploadFromDir('models', 'models');
+    // await client.rename('index.js', 'index0.js');
+    // console.log(await client.list());
+  } catch (err) {
+    console.log(err);
+  }
+  client.close();
+}
