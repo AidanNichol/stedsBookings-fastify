e3d96b7e6885700c02a1f0002bbdc6b181bb764b
Aidan Nichol <aidan@nicholware.co.uk>
2021-07-08 13:22:14 +0100

    cleanup db settings remove legacy scopes

--- models/index.js
+++ models/index.js
@@ -1,16 +1,10 @@
-// const fs = require('fs');
-// const path = require('path');
 const Sequelize = require('sequelize');
-// const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require('../config/config.json')[env];
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+let sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: './database.sqlite',
+  logging: false,
+});
 const ts = { timestamp: false, createdAt: false, updatedAt: false };
 const Walk = sequelize.define('Walk', require('./walk.js'), ts);
 const Booking = sequelize.define('Booking', require('./booking.js'), ts);
@@ -71,15 +65,15 @@ const db = {
   User,
 };
 // add scopes
-require('../junk/scopes/bookingLogScopes')(db);
-require('../junk/scopes/bookingScopes')(db);
-require('../junk/scopes/walkScopes')(db);
-require('../junk/scopes/paymentScopes')(db);
-require('../junk/scopes/refundScopes')(db);
-require('../junk/scopes/accountScopes')(db);
-require('../junk/scopes/memberScopes')(db);
-require('../junk/scopes/allocationScopes')(db);
-require('../junk/scopes/bankingScopes')(db);
+// require('../junk/scopes/bookingLogScopes')(db);
+// require('../junk/scopes/bookingScopes')(db);
+// require('../junk/scopes/walkScopes')(db);
+// require('../junk/scopes/paymentScopes')(db);
+// require('../junk/scopes/refundScopes')(db);
+// require('../junk/scopes/accountScopes')(db);
+// require('../junk/scopes/memberScopes')(db);
+// require('../junk/scopes/allocationScopes')(db);
+// require('../junk/scopes/bankingScopes')(db);
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
