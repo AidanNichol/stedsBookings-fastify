0e8c52fecb672d001253bfc95651e30ce0f0bfdd
Aidan Nichol <aidan@nicholware.co.uk>
2021-09-15 12:00:26 +0100

    fix today function not changing on day change

--- server/routes/bookings/dateFns.js
+++ server/routes/bookings/dateFns.js
@@ -25,9 +25,9 @@ exports.datetimeNextSec = (oldDate, inc = 1) => {
   return formatISOdate(addSeconds(parseISO(oldDate.slice(0, -4)), inc));
 };
 
-let today = new Date();
-// today = parseISO('2020-03-18');
 exports.todaysDate = () => {
+  let today = new Date();
+  // today = parseISO('2020-03-18');
   return formatDate(today);
 };
 
@@ -35,6 +35,9 @@ exports.getTimestamp = function getTimestamp() {
   return formatISOdate(new Date());
 };
 exports.currentSubsYear = () => {
+  let today = new Date();
+  // today = parseISO('2020-03-18');
+
   // let tday = formatDate(today)
   let year = format(today, 'yyyy');
   // const tdy = parseISO(today());
