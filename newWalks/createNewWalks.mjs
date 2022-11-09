a6bee8e7e27e85da727bf307e2802a55e327b69b
Aidan Nichol <aidan@nicholware.co.uk>
2022-11-09 16:21:45 +0000

    added prog to create new walks from a csv file

---
+++ newWalks/createNewWalks.mjs
@@ -0,0 +1,77 @@
+import models from '../models/index.js';
+import { parseISO, format, addDays } from 'date-fns';
+import { parse } from 'csv-parse/sync';
+import jetpack from 'fs-jetpack';
+import fetch from 'node-fetch';
+const progdata = jetpack.read('setup/prog2023.csv');
+
+async function create() {
+  const prog = parse(progdata, {
+    columns: ['date', null, 'venue'],
+    skip_empty_lines: true,
+    relax_column_count_less: true,
+    relax_column_count_more: true,
+    trim: true,
+  });
+  console.log(prog);
+  let lastDate = null;
+  for (const walk of prog) {
+    let { date, venue } = walk;
+    if (date !== '') {
+      lastDate = parseISO(date);
+    } else {
+      lastDate = addDays(lastDate, 14);
+      date = format(lastDate, 'yyyy-MM-dd');
+    }
+    console.log('walk======>', walk);
+    let walkId = `W${date}`;
+    let lastCancel = addDays(parseISO(`${date} 18:00`), -6);
+    let firstBooking = addDays(parseISO(`${date} 00:01`), -45);
+    console.log(format(lastCancel, 'EEEE'));
+    console.log(format(firstBooking, 'EEEE'));
+    lastCancel = format(addDays(parseISO(`${date} 18:00`), -6), 'yyyy-MM-dd HH:mm');
+    firstBooking = format(addDays(parseISO(`${date} 00:01`), -45), 'yyyy-MM-dd');
+    const walkData = {
+      walkId,
+      venue,
+      capacity: 51,
+      fee: 12.5,
+      lastCancel,
+      firstBooking,
+      closed: false,
+    };
+    let res = await models.Walk.create(walkData);
+    console.log(res);
+    // const patch = { op: 'add', path: ['Walk', walkId], value: walkData };
+    // res = await postData([patch]);
+    // console.log('appliedPatches', patch, res);
+    break;
+  }
+}
+create();
+const db = 'https://www.stedwardsfellwalkers.co.uk/bookingsServer/';
+
+const postRequest = async (url, data) => {
+  try {
+    console.log('postRequest', data);
+    const res = await fetch(db + url, {
+      method: 'POST',
+      mode: 'cors',
+      cache: 'no-cache',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data), // body data type must match "Content-Type" header
+    });
+    console.log('post result', res);
+    let body = await res.json();
+    console.log(`postRequest ${url} returned:`, body);
+    // console.log(`fetchData ${url} post:`, body);
+    return body;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const postData = (data) => postRequest('bookings/patches', data);
